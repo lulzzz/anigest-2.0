658a160403cb1ecc2ff763f4c12cef83210180f3
raquelmeloalmeida <32928140+raquelmeloalmeida@users.noreply.github.com>
2019-10-18 12:35:29 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://anieca.herokuapp.com/api/'
+  apiUrl: 'https://anieca.herokuapp.com/api/'
 
   //apiUrl: 'http://192.168.0.106:4200/api/'
 };
