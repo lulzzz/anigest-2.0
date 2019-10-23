dd3bff72f44d297aafdae257d1d03eb18fb66d67
Ruben Lebre <ruben.lebre@gmail.com>
2019-10-23 20:52:57 +0100

    Updated border colors

--- src/app/reservations/horario/schedule-utils/colors.ts
+++ src/app/reservations/horario/schedule-utils/colors.ts
@@ -1,36 +1,36 @@
 export const colors: any = {
   red: { // Sem significado originalmente, agora sem exames marcados
-    primary: '#cc6500',
+    primary: '#777',
     secondary: '#ff614f'
   },
   blue: { // Exames já efetuados
-    primary: '#1e90ff',
+    primary: '#777',
     secondary: '#c0ffff'
   },
   yellow: { // Sem exames marcados, agora pauta completa
-    primary: '#777777',
+    primary: '#777',
     // secondary: '#ffe0a0'
     secondary: '#feffa0'
   },
   white: { // Interdito a marcações
     // primary: '#777777',
-    primary: '#eee',
+    primary: '#999',
     // secondary: '#FFFFFF'
-    secondary: '#DDD'
+    secondary: '#eeee'
   },
   orange: { // Com alguns exames marcados
-    primary: '#e37608',
+    primary: '#777',
     // secondary: '#ffb070'
     secondary: '#ffe0a0'
   },
   darkorange: { // Pauta completa, agora sem significado
-    primary: '#cc6500',
+    primary: '#777',
     // secondary: '#eb9752'
     // secondary: '#ffb070'
     secondary: '#ff614f'
   },
   green: { // Com examinadores atribuídos, agora timeslots atuais(?)
-    primary: '#4ad452',
+    primary: '#777',
     secondary: '#c0ffc0'
   },
   doublecolor1: {},
