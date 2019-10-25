3dfb2eddd133e949734ed005662b2bbc959a70ee
Ruben Lebre <ruben.lebre@gmail.com>
2019-10-25 17:16:14 +0100

    Changed the way timeslots are received
    Instead of sending the week number, the beginning and ending date of the week are sent.

--- src/app/reservations/horario/services/timeslot.service.ts
+++ src/app/reservations/horario/services/timeslot.service.ts
@@ -25,17 +25,33 @@ export class TimeslotService {
   }
 
   getTimeslots(date: Date): Observable<Timeslot[]> {
-    let week = this.getWeekNumber(date)
-    return this.http.get<Timeslot[]>(`${this.url}?week=${(week[1])}&year=${week[0]}`)
+    let week = this.getWeekDays(date)
+    // return this.http.get<Timeslot[]>(`${this.url}?week=${(week[1])}&year=${week[0]}`)
     // return this.http.get<Timeslot[]>(`${this.url}?week=31&year=2019`, httpOptions)
+    return this.http.get<Timeslot[]>(`${this.url}?begin=${(week[1])}&end=${week[2]}&year=${week[0]}`)
   }
 
   getWeekNumber(d) {
     d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
     d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    console.log(d)
+    let startDate = new Date(d.getTime())
     var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
     var weekNo = Math.ceil(( ( (d - +yearStart) / 86400000) + 1)/7);
     return [d.getUTCFullYear(), weekNo];
+  }
+
+  getWeekDays(d) {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    console.log(d)
+    let startDate = new Date(d.getTime())
+    let endDate = new Date(d.getTime())
+    startDate.setUTCDate(startDate.getUTCDate() - 3)
+    let startDateFormatted = `${startDate.getUTCFullYear()}-${(startDate.getUTCMonth()+1)}-${startDate.getUTCDate()}`
+    endDate.setUTCDate(endDate.getUTCDate() + 3)
+    let endDateFormatted = `${endDate.getUTCFullYear()}-${(endDate.getUTCMonth()+1)}-${endDate.getUTCDate()}`
+    return [d.getUTCFullYear(), startDateFormatted, endDateFormatted]
   }
 
   updateTimeslot(idTimeslot, newData) {
