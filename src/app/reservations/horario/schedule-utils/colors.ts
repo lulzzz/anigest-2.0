63f31ae15b1099c035bf1363fe72a0dd78f35ed7
Ruben Lebre <ruben.lebre@gmail.com>
2019-11-22 12:15:18 +0000

    Update colors.ts

--- src/app/reservations/horario/schedule-utils/colors.ts
+++ src/app/reservations/horario/schedule-utils/colors.ts
@@ -1,38 +1,41 @@
 export const colors: any = {
-  red: { // Sem significado originalmente, agora sem exames marcados
-    primary: '#999',
+  red: { // Timeslot (incomplete or not) with reservations only
+    primary: '#777',
     secondary: '#ff614f'
   },
-  blue: { // Exames já efetuados
-    primary: '#999',
+  blue: { // Past timeslots
+    primary: '#777',
     secondary: '#c0ffff'
   },
-  yellow: { // Sem exames marcados, agora pauta completa
-    primary: '#999',
+  yellow: { // Timeslot with bookings but not full
+    primary: '#777',
     // secondary: '#ffe0a0'
     secondary: '#feffa0'
   },
-  white: { // Interdito a marcações
+  white: { // Empty timeslots
     // primary: '#777777',
     primary: '#999',
     // secondary: '#FFFFFF'
     secondary: '#eeee'
   },
-  orange: { // Com alguns exames marcados
-    primary: '#999',
+  orange: { // Timeslot filled with bookings only (?)
+    primary: '#777',
     // secondary: '#ffb070'
     secondary: '#ffe0a0'
   },
-  darkorange: { // Pauta completa, agora sem significado
-    primary: '#999',
+  darkorange: { // Timeslot filled with bookings only (?)
+    primary: '#777',
     // secondary: '#eb9752'
     // secondary: '#ffb070'
-    secondary: '#ff614f'
+    secondary: '#ffb070'
   },
-  green: { // Com examinadores atribuídos, agora timeslots atuais(?)
-    primary: '#999',
+  green: { // Present timeslots
+    primary: '#777',
     secondary: '#c0ffc0'
   },
-  doublecolor1: {},
+  doublecolor1: { // Timeslot with both reservations and bookings
+    primary: '#777',
+    secondary: '#D8BFD8'
+  },
   doublecolor2: {}
 };
